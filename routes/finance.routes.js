2d868dc8b59ee475135d21af3191f3fbefcf7136
sijincodes <sijinmathews03@gmail.com>
2022-12-14 21:47:10 +0100

    api for analysis implemented

--- routes/finance.routes.js
+++ routes/finance.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const moment = require("moment");
+const mongoose = require("mongoose");
 const Transaction = require("../models/Transaction.model");
 const { isAuthenticated } = require("./../middleware/jwt.middleware");
 
@@ -93,6 +94,138 @@ router.delete(
     try {
       const transaction = await Transaction.findByIdAndRemove(transactionid);
       res.status(200).json(transaction);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//Fetch grouped by transactions
+router.get(
+  "/analysis/overview/:overviewType",
+  isAuthenticated,
+  async (req, res, next) => {
+    const { overviewType } = req.params;
+    const { TransactionCreatedMonth, TransactionCreatedYear } = req.query;
+
+    try {
+      const transactions = await Transaction.aggregate([
+        {
+          $match: {
+            transactionType: overviewType,
+            userId: mongoose.Types.ObjectId(req.payload._id),
+            TransactionCreatedMonth: TransactionCreatedMonth,
+            TransactionCreatedYear: Number(TransactionCreatedYear),
+          },
+        },
+        {
+          $project: {
+            TransactionCreatedDate: 1,
+            transactionAmount: 1,
+            _id: 0,
+          },
+        },
+        {
+          $group: {
+            _id: "$TransactionCreatedDate",
+            total: {
+              $sum: "$transactionAmount",
+            },
+          },
+        },
+        {
+          $sort: {
+            _id: 1,
+          },
+        },
+      ]);
+      res.status(200).json(transactions);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//Fetch grouped by categories
+router.get("/analysis/categories", isAuthenticated, async (req, res, next) => {
+  const { TransactionCreatedMonth, TransactionCreatedYear } = req.query;
+
+  try {
+    const transactions = await Transaction.aggregate([
+      {
+        $match: {
+          TransactionCreatedMonth: TransactionCreatedMonth,
+          TransactionCreatedYear: Number(TransactionCreatedYear),
+          transactionType: "expense",
+          userId: mongoose.Types.ObjectId(req.payload._id),
+        },
+      },
+      {
+        $project: {
+          categoryId: 1,
+          transactionAmount: 1,
+          _id: 0,
+        },
+      },
+      {
+        $group: {
+          _id: "$categoryId",
+          total: {
+            $sum: "$transactionAmount",
+          },
+        },
+      },
+      {
+        $sort: {
+          total: -1,
+        },
+      },
+    ]);
+    res.status(200).json(transactions);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Fetch grouped by categories
+router.get(
+  "/analysis/totalExpense/:transactionType",
+  isAuthenticated,
+  async (req, res, next) => {
+    const { TransactionCreatedYear } = req.query;
+    const { transactionType } = req.params;
+
+    try {
+      const transactions = await Transaction.aggregate([
+        {
+          $match: {
+            TransactionCreatedYear: Number(TransactionCreatedYear),
+            transactionType: transactionType,
+            userId: mongoose.Types.ObjectId(req.payload._id),
+          },
+        },
+        {
+          $project: {
+            TransactionCreatedMonth: 1,
+            transactionAmount: 1,
+            _id: 0,
+          },
+        },
+        {
+          $group: {
+            _id: "$TransactionCreatedMonth",
+            total: {
+              $sum: "$transactionAmount",
+            },
+          },
+        },
+        {
+          $sort: {
+            _id: -1,
+          },
+        },
+      ]);
+      res.status(200).json(transactions);
     } catch (error) {
       next(error);
     }
